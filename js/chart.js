5f836ff33dbb2a4ebe9699b3ddcfa93a6db9204d
Romit Choudhary <choudharyromit@gmail.com>
2018-02-18 00:19:55 +0530

    minor changes

--- js/chart.js
+++ js/chart.js
@@ -56,6 +56,10 @@ drawChart(".chart-6", data2, 0.7);
 
 function drawChart(chartClassName, data, spacing){
 
+    var div = d3.select("body").append("div")
+    .attr("class", "custom-tooltip text-center")
+    .style("opacity", 0);
+
     var divWidth = $(chartClassName).width();
 
     var margin = {top: 20, right: 20, bottom: 30, left: 20},
@@ -87,6 +91,21 @@ function drawChart(chartClassName, data, spacing){
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
+        .on("click", function(d) {
+           div.transition()
+             .duration(200)
+             .style("opacity", 1);
+           div.html(
+            "<p><strong>Politeness Received</strong></p><div><span class='data-comparison-item-icon blue'></span><span class='data-comparison-item-percent'><strong>73%</strong></span><span class='data-comparison-item-group'>Male</span></div><div><span class='data-comparison-item-icon red'></span><span class='data-comparison-item-percent'><strong>63%</strong></span><span class='data-comparison-item-group'>Female</span></div>"
+            )
+             .style("left", (d3.event.pageX - 80) + "px")
+             .style("top", (d3.event.pageY - 100) + "px");
+           })
+         .on("mouseout", function(d) {
+           div.transition()
+             .duration(500)
+             .style("opacity", 0);
+           })
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     var ageNames = d3.keys(data[0]).filter(function(key) { return key !== "Groups"; });
@@ -143,8 +162,7 @@ var data =  [
   [{'x':0,'y':0},{'x':2,'y':5},{'x':3,'y':10},{'x':4,'y':0},{'x':5,'y':6},{'x':6,'y':11},{'x':7,'y':9},{'x':8,'y':4},{'x':9,'y':11},{'x':10,'y':2},{'x':11,'y':6},{'x':12,'y':11}],
   [{'x':0,'y':1},{'x':2,'y':6},{'x':3,'y':11},{'x':4,'y':1},{'x':5,'y':7},{'x':6,'y':12},{'x':7,'y':8},{'x':8,'y':3},{'x':9,'y':13},{'x':10,'y':3},{'x':11,'y':7},{'x':12,'y':9}],
   [{'x':0,'y':2},{'x':2,'y':7},{'x':3,'y':12},{'x':4,'y':2},{'x':5,'y':8},{'x':6,'y':13},{'x':7,'y':7},{'x':8,'y':2},{'x':9,'y':4},{'x':10,'y':7},{'x':11,'y':8},{'x':12,'y':4}],
-  [{'x':0,'y':3},{'x':2,'y':8},{'x':3,'y':13},{'x':4,'y':3},{'x':5,'y':9},{'x':6,'y':14},{'x':7,'y':6},{'x':8,'y':1},{'x':9,'y':7},{'x':10,'y':9},{'x':11,'y':3},{'x':12,'y':7}],
-  [{'x':0,'y':4},{'x':2,'y':9},{'x':3,'y':14},{'x':4,'y':4},{'x':5,'y':10},{'x':6,'y':15},{'x':7,'y':5},{'x':8,'y':0},{'x':9,'y':8},{'x':10,'y':5},{'x':11,'y':4},{'x':12,'y':10}]
+  [{'x':0,'y':3},{'x':2,'y':8},{'x':3,'y':13},{'x':4,'y':3},{'x':5,'y':9},{'x':6,'y':14},{'x':7,'y':6},{'x':8,'y':1},{'x':9,'y':7},{'x':10,'y':9},{'x':11,'y':3},{'x':12,'y':7}]
 ];
 
 drawMultiLineChart(".chart-7", data);
@@ -162,7 +180,7 @@ function drawMultiLineChart(chartClassName, data){
   //************************************************************
   // Create Margins and Axis and hook our zoom function
   //************************************************************
-  var margin = {top: 20, right: 30, bottom: 30, left: 50},
+  var margin = {top: 20, right: 10, bottom: 30, left: 15},
       width = divWidth - margin.left - margin.right,
       height = 300 - margin.top - margin.bottom;
     
@@ -187,16 +205,7 @@ function drawMultiLineChart(chartClassName, data){
     .tickSize(-width)
     .tickSubdivide(true)  
       .orient("left");
-    
-  var zoom = d3.behavior.zoom()
-      .x(x)
-      .y(y)
-      .scaleExtent([1, 10])
-      .on("zoom", zoomed);  
-    
-    
-   
-    
+  
     
   //************************************************************
   // Generate our SVG object
@@ -227,9 +236,6 @@ function drawMultiLineChart(chartClassName, data){
     .attr("height", height);
     
     
-    
-    
-    
   //************************************************************
   // Create D3 line object and draw data on our SVG object
   //************************************************************
@@ -237,7 +243,7 @@ function drawMultiLineChart(chartClassName, data){
       .interpolate("linear")  
       .x(function(d) { return x(d.x); })
       .y(function(d) { return y(d.y); });   
-    
+
   svg.selectAll('.line')
     .data(data)
     .enter()
@@ -247,56 +253,14 @@ function drawMultiLineChart(chartClassName, data){
     .attr('stroke', function(d,i){      
       return colors[i%colors.length];
     })
-      .attr("d", line);   
-    
-    
-    
-    
-  //************************************************************
-  // Draw points on SVG object based on the data given
-  //************************************************************
-  // var points = svg.selectAll('.dots')
-  //   .data(data)
-  //   .enter()
-  //   .append("g")
-  //     .attr("class", "dots")
-  //   .attr("clip-path", "url(#clip)"); 
-   
-  // points.selectAll('.dot')
-  //   .data(function(d, index){     
-  //     var a = [];
-  //     d.forEach(function(point,i){
-  //       a.push({'index': index, 'point': point});
-  //     });   
-  //     return a;
-  //   })
-  //   .enter()
-  //   .append('circle')
-  //   .attr('class','dot')
-  //   .attr("r", 2.5)
-  //   .attr('fill', function(d,i){  
-  //     return colors[d.index%colors.length];
-  //   })  
-  //   .attr("transform", function(d) { 
-  //     return "translate(" + x(d.point.x) + "," + y(d.point.y) + ")"; }
-  //   );
-    
-   
-    
-    
-    
-    
-  //************************************************************
-  // Zoom specific updates
-  //************************************************************
-  function zoomed() {
-    svg.select(".x.axis").call(xAxis);
-    svg.select(".y.axis").call(yAxis);   
-    svg.selectAll('path.line').attr('d', line);  
-   
-    points.selectAll('circle').attr("transform", function(d) { 
-      return "translate(" + x(d.point.x) + "," + y(d.point.y) + ")"; }
-    );  
-  }
+    .style("stroke-dasharray", function(d,i){      
+      if(i%colors.length == 2 || i%colors.length == 3){
+        return "3";
+      }
+      else{
+        return "0";
+      }
+    })
+      .attr("d", line); 
 }
 
